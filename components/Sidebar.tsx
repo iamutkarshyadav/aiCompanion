49af83076f20b1764cb069ec988f040bd519493b
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-14 22:38:07 +0530

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,53 @@
-// Make sure to import React
 "use client"
-import React from 'react';  // Make sure to import React
+import { cn } from '@/lib/utils';
+
+import { Home, Plus, Settings } from 'lucide-react';
+import { usePathname } from 'next/navigation';
+import React from 'react';  
 
 const Sidebar = () => {
+  const pathname = usePathname();  // Invoke usePathname as a function
+  const routes = [
+    {
+      icon: Home,
+      href: '/',
+      label: 'Home',
+      pro: false,
+    },
+    {
+      icon: Plus,
+      href: '/companies/new',
+      label: 'Create',
+      pro: false,
+    },
+    {
+      icon: Settings,
+      href: '/settings',
+      label: 'Settings',
+      pro: false,
+    }
+  ]
   return (
     <div className="flex flex-col h-full text-primary bg-secondary">
       <div className="p-3 flex-1 justify-center">
         <div className="space-y-2">
-          {/* Content of your Sidebar, e.g., routes */}
-          Routes
+          {routes.map((route, i) => (
+            <div key={route.href}
+              className={cn(
+                "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href && "bg-primary/10 text-primary"
+              )}
+            >
+              <div className=' flex flex-col gap-y-2 items-center flex-1'>
+              </div>
+              <route.icon className='h-5 w-5'/>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
 };
 
-export default Sidebar;  // Export the component
+export default Sidebar;
+ 
