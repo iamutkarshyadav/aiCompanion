87994c47f104ae60d3ebf865a0598a3afa329a52
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-15 00:23:54 +0530

    still on backend

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,17 +1,36 @@
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { ChangeEventHandler, useEffect, useState } from "react";
 import { useDebounce } from "@/hooks/use-debounce";
+
+
+import qs from "query-string";
+
 
 export const SearchInput = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const categoryId = searchParams.get("categoryId");
   const name = searchParams.get("name");
-
   const [value, setValue] = useState(name || "");
   const debouncedValue = useDebounce<string>(value, 500);
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  setValue(e.target.value);
+    };
+    
+    useEffect(() => {
+        const query = {
+            name: debouncedValue,
+            categoryId: categoryId 
+        };
+        const url = qs.stringifyUrl({
+            url: window.location.href,
+            query :query,
+        }, { skipEmptyString: true, skipNull: true });
+        router.push(url)
+    }, [debouncedValue, categoryId, router]);
+
 
   return (
     <div className="relative">
