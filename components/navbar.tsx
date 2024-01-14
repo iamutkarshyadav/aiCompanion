a5c00cc161b0e1f75667ca3a0125c01b2d114827
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-14 20:45:01 +0530

    navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,43 @@
-"use client"
- 
-import React from 'react'
+"use client";
 
-const navbar = () => {
+import React from 'react';
+import { Menu, Sparkle } from 'lucide-react';
+import Link from 'next/link';
+import { Poppins } from 'next/font/google';
+import { UserButton } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+
+import { cn } from '@/lib/utils';
+
+
+
+const font = Poppins({
+  weight: '600',
+  subsets: ['latin'],
+})
+const Navbar = () => {
   return (
-    <div>navbar!</div>
-  )
-}
+    <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary'>
+      <div className='flex items-center'>
+        <Menu className='block md:hidden' />
+        <Link href='/'>
+          <h1 className={cn(
+            'hidden md:block text-xl md:text-3xl font-bold text-primary',
+            font.className
+          )}>
+            companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className='flex items-center gap-x-3'>
+        <Button size= "sm">
+          Upgrade
+          <Sparkle className=' h-4 w-4 fill-white text-white ml-2'/>
+        </Button>
+        <UserButton />
+      </div>
+    </div>
+  );
+};
 
-export default navbar
+export default Navbar;
