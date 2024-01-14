be80d93e2cbc100f9460d557dc749e6e34f5ddab
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-15 00:28:49 +0530

    search

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,12 +1,12 @@
+// components/search-input.tsx
+"use client"; // Place the "use client" pragma at the beginning of the file
+
 import { Search } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { useRouter, useSearchParams } from "next/navigation";
 import { ChangeEventHandler, useEffect, useState } from "react";
 import { useDebounce } from "@/hooks/use-debounce";
-
-
-import qs from "query-string";
-
+import qs from "query-string"; // Import qs
 
 export const SearchInput = () => {
   const router = useRouter();
@@ -15,28 +15,33 @@ export const SearchInput = () => {
   const name = searchParams.get("name");
   const [value, setValue] = useState(name || "");
   const debouncedValue = useDebounce<string>(value, 500);
+  
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-  setValue(e.target.value);
-    };
-    
-    useEffect(() => {
-        const query = {
-            name: debouncedValue,
-            categoryId: categoryId 
-        };
-        const url = qs.stringifyUrl({
-            url: window.location.href,
-            query :query,
-        }, { skipEmptyString: true, skipNull: true });
-        router.push(url)
-    }, [debouncedValue, categoryId, router]);
+    setValue(e.target.value);
+  };
 
+  useEffect(() => {
+    const query = {
+      name: debouncedValue,
+      categoryId: categoryId,
+    };
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query: query,
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+    router.push(url);
+  }, [debouncedValue, categoryId, router]);
 
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
       <Input
-        placeholder="Search"
+        onChange={onChange}
+        value={value}
+        placeholder="Search..."
         className="pl-10 bg-primary/10"
       />
     </div>
