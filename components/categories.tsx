4a72bd7d37f2dcd709e0eb80397efe9725560797
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-15 22:53:37 +0530

    fixed up db

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import qs from "query-string";
-import { category } from "@prisma/client"
+import { Category } from "@prisma/client"
 import { useRouter, useSearchParams } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 
 interface CategoriesProps {
-  data: category[]
+  data: Category[]
 }
 
 export const Categories = ({
