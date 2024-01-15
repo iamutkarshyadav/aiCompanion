8dc72549bdc1d87e75e52affa650a38cb8516eef
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-15 15:12:18 +0530

    added categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -28,6 +28,27 @@ export const Categories = ({
             >
                 Newest
             </button>
+            {data.map((item) => (
+                <button
+                    key = {item.id}
+                    className={cn(`
+             flex 
+             items-center
+             text-xs
+             md:text-sm
+             px-2
+             md:px-4
+             py-2
+             md:py-3
+             rounded-md
+             bg-primary/10
+             hover:opacity-75
+             transition
+            `)}
+            >
+               {item.name}
+            </button>
+            ))}
         </div>
     )
 }
