27dc86e2b615a5d6b368c4474949203b88256642
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-15 23:36:26 +0530

    companionId

---
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Category, Companion } from "@prisma/client"
+
+interface CompanionFormProps {
+    initialData: Companion | null
+    categories: Category[];
+    }
+
+export const CompanionForm = ({
+    categories, 
+    initialData
+}: CompanionFormProps) => {
+  return (
+      <div>
+          companion-form
+      </div>
+  )
+}
--- app/(root)/(routes)/companion/[companionId]/page.tsx
+++ app/(root)/(routes)/companion/[companionId]/page.tsx
@@ -1,8 +1,30 @@
-const CompanionIdPage = () => {
+import prismadb from "@/lib/prismadb";
+import { CompanionForm } from "./components/companion-form";
+interface CompanionIdPageProps {
+    params: {
+        companionId: string;
+    }
+}
+
+const CompanionIdPage = async ({
+    params
+}: CompanionIdPageProps) => {
+    // To do check subs 
+     const companion = await prismadb.companion.findUnique({
+    where: {
+      id: params.companionId,
+    }
+     });
+    
+    const categories = await prismadb.category.findMany();
+
+    
     return (
-        <div>
-            Hello Comapnion Id
-        </div>
+        <CompanionForm
+        initialData = { companion }
+        categories = { categories }
+         />
+
     )
 }
 export default CompanionIdPage;
