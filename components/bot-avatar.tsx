f23646e5a63fe824dcd8eded9f3c842bc7fa1567
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-16 20:55:36 +0530

    chat page

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
-export const BotAvatar = () => {
-  return (
-    <div>bot-avatar</div>
+interface BotAvatarProps {
+    src: string;
+};
+
+
+export const BotAvatar = ({
+    src
+}:BotAvatarProps) => {
+    return (
+        <Avatar className="h-12 w-12">
+            <AvatarImage
+            src={src}
+            />
+      </Avatar>
   )
 }
--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,8 +1,23 @@
 import { Companion, Message } from "@prisma/client";
 import { Button } from "@/components/ui/button";
-import { Bot, ChevronLeft } from "lucide-react";
+import {
+  Bot,
+  ChevronLeft,
+  Edit,
+  MessageSquare,
+  MoreVertical,
+  Trash} from "lucide-react";
 import { useRouter } from "next/navigation";
 import { BotAvatar } from "@/components/bot-avatar";
+import { useUser } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+
 
 interface ChatHeaderProps {
   companion: Companion & {
@@ -15,6 +30,22 @@ interface ChatHeaderProps {
 
 export const ChatHeader = ({ companion }: ChatHeaderProps) => {
   const router = useRouter();
+  const { user } = useUser();
+  const { toast } = useToast()
+
+  const onDelete =async () => {
+    try {
+      await axios.delete(`/api/companions/${companion.id}`)
+      toast({
+        description:"success"
+      })
+    } catch (error) {
+      toast({
+        description: "Something went wrong",
+        variant: "destructive"
+      })
+    }
+  }
 
   return (
     <div className="flex w-full justify-between items-center border-b border-primary/10 pb-4">
@@ -26,8 +57,51 @@ export const ChatHeader = ({ companion }: ChatHeaderProps) => {
         >
           <ChevronLeft className="h-8 w-8" />
         </Button>
-        <BotAvatar/>
+        <BotAvatar
+          src={companion.src}
+        />
+        <div className="flex flex-col gap-y-2">
+          <div className="flex items-center gap-x-2">
+            <p className="font-bold">
+              {companion.name}
+            </p>
+            <div className="flex items-center text-xs text-muted-foreground">
+              <MessageSquare
+                className="w-3 h-3 mr-1"
+              />
+              {companion._count.messages}
+            </div>
+          </div>
+          <p className=" text-xs text-muted-foreground">
+            Created by {companion.userName}
+          </p>
+        </div>
       </div>
+      {user?.id === companion.userId && (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="secondary"
+              size="icon">
+              <MoreVertical/>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => router.push(`/companion/${companion.id}`)}>
+              <Edit
+                className="w-4 h-4 mr-2"
+              />
+              Edit
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick= {onDelete}>
+              <Trash
+                className="w-4 h-4 mr-2"
+              />
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )}
     </div>
   );
 };
