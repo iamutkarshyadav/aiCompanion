814b772596358008d1a0b9a61d051cef874f6b4d
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-16 16:08:46 +0530

    homepage done

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,37 @@
 import { Categories } from "@/components/categories";
 import { SearchInput } from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
-//import { UserButton } from "@clerk/nextjs";
+import { Companions } from "@/components/companions";
 
-const RootPage: React.FC = async () => {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async ({
+    searchParams 
+}: RootPageProps) => {
+    const data = await prismadb.companion.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name,
+            }
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+        include: {
+            _count: {
+                select: {
+                    messages:true
+                }
+            }
+        }
+    })
+
     const categories = await prismadb.category.findMany({
 
     })
@@ -11,6 +39,7 @@ const RootPage: React.FC = async () => {
         <div className=" h-full p-4 space-y-2">
             <SearchInput />
             <Categories data={categories} />
+            <Companions data={data} />
         </div>
     )
 }
