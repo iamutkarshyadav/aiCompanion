aa3e6db7fdfc62039f944436fb808fbf224ff687
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-17 01:06:51 +0530

    copy message button and aavatar

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,53 +1,66 @@
-"use client"
+"use client";
 
 import { BeatLoader } from "react-spinners";
+import { Copy } from "lucide-react";
 import { useTheme } from "next-themes";
-import { useToast } from "./ui/use-toast";
+
 import { cn } from "@/lib/utils";
-import { BotAvatar } from "@/components/bot-avatar";
+import { BotAvatar } from "@/components/bot-avatar"
+import { UserAvatar } from "@/components/user-avatar";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 export interface ChatMessageProps {
-    role: "system" | "user",
-    content?: string;
-    isLoading: boolean;
-    src?: string;
-};
+  role: "system" | "user",
+  content?: string;
+  isLoading?: boolean;
+  src?: string;
+}
 
 export const ChatMessage = ({
-    role,
-    content,
-    isLoading,
-    src
+  role,
+  content,
+  isLoading,
+  src
 }: ChatMessageProps) => {
-    
-    const { toast } = useToast();
-    const { theme } = useTheme();
-
-    const onCopy = () => {
-        if (!content) {
-            return;
-        }
-        toast({
-            description: "Message copied to clipboard",
-        });
+  const { toast } = useToast();
+  const { theme } = useTheme();
+  
+  const onCopy = () => {
+    if (!content) {
+      return;
     }
 
-    return (
-        <div className={cn(
-            "group flex items-start gap-x-3 py-4 w-full",
-            role === "user" && "justify-end"
-        )} >
-            {role !== 'user' && src && <BotAvatar src={src} />}
-            <div className="rounded-md px-4 py-2 max-w-sm text-sm bg-primary/10">
-                {isLoading
-                    ? <BeatLoader
-                        size={5}
-                        color={theme === "light" ? "black" : "white"}
-                    />
-                    :content
-                }
+    navigator.clipboard.writeText(content);
+    toast({
+      description: "Message copied to clipboard.",
+      duration: 3000,
+    })
+  }
 
-            </div>
-        </div>
+  return (
+    <div className={cn(
+      "group flex items-start gap-x-3 py-4 w-full",
+      role === "user" && "justify-end"
+    )}>
+      {role !== "user" && src && <BotAvatar src={src} />}
+      <div className="rounded-md px-4 py-2 max-w-sm text-sm bg-primary/10">
+        {isLoading 
+          ? <BeatLoader color={theme === "light" ? "black" : "white"} size={5} /> 
+          : content
+        }
+      </div>
+      {role === "user" && <UserAvatar />}
+      {role !== "user" && !isLoading && (
+        <Button 
+          onClick={onCopy} 
+          className="opacity-0 group-hover:opacity-100 transition" 
+          size="icon"
+          variant="ghost"
+        >
+          <Copy className="w-4 h-4" />
+        </Button>
+      )}
+    </div>
   )
 }
