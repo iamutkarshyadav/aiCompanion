2cefb2dc3e3bb8dfc4b81680f1cce0d48843cc5a
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-18 05:00:54 +0530

    build error fixed

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -8,17 +8,19 @@ import {
 
 import Sidebar from "@/components/Sidebar";
 
-export const MobileSidebar = () => {
+export const MobileSidebar = ({
+  isPro
+}: {
+  isPro: boolean;
+}) => {
   return (
-    <div>
-      <Sheet>
-        <SheetTrigger className="md:hidden pr-4">
-          <Menu />
-        </SheetTrigger>
-        <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-          <Sidebar />
-        </SheetContent>
-      </Sheet>
-    </div>
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
+        <Sidebar isPro={isPro} />
+      </SheetContent>
+    </Sheet>
   );
 };
