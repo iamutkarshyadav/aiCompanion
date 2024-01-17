1c003385ac754564141c0c8b12b03e76c48660d0
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-18 02:31:25 +0530

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider'
 import { cn } from '@/lib/utils'
 import { Toast } from '@radix-ui/react-toast'
 import { Toaster } from '@/components/ui/toaster'
+import { ProModal } from '@/components/pro-modal'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en"suppressHydrationWarning>
         <body className={ cn ("bg-secondary" ,inter.className)}>
           <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+            <ProModal/>
             {children}
             <Toaster/>
            </ThemeProvider>
