2f59aeb4a704a1c08d2a2efce7be594014c70a8f
iamutkarshyadav <iamutkarshyadav1@gmail.com>
2024-01-18 05:03:06 +0530

    another error fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,34 +22,28 @@ const Navbar = ({
   isPro
 }: NavbarProps) => {
   const proModal = useProModal();
-  
-  return (
-    <div className='fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16'>
-      <div className='flex items-center'>
-        <MobileSidebar/>
-        <Link href='/'>
-          <h1 className={cn(
-            'hidden md:block text-xl md:text-3xl font-bold text-primary',
-            font.className
-          )}>
+
+  return ( 
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <MobileSidebar isPro={isPro} />
+        <Link href="/">
+          <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>
             companion.ai
           </h1>
         </Link>
       </div>
-      <div className='flex items-center gap-x-3'>
-        {!isPro &&(
-        <Button
-          onClick={proModal.onOpen}
-          variant='premium' size="sm">
-          Upgrade
-          <Sparkle className=' h-4 w-4 fill-white text-white ml-2'/>
-        </Button>
-          )}
-        <ModeToggle/>
-        <UserButton afterSignOutUrl="/"/>
+      <div className="flex items-center gap-x-3">
+        {!isPro && (
+          <Button onClick={proModal.onOpen} size="sm" variant="premium">
+            Upgrade
+            <Sparkle className="h-4 w-4 fill-white text-white ml-2" />
+          </Button>
+        )}
+        <ModeToggle />
+        <UserButton afterSignOutUrl="/" />
       </div>
     </div>
   );
-};
-
+}
 export default Navbar;
